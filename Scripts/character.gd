35bd51c06ca2b4d023dee2d5cf677d51ce874385
HX06-dev <hugo.yuguo.xie@gmail.com>
2025-08-01 13:27:54 -0400

    Tileset and key scenes

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var SPEED = 100.0
+@export var JUMP_VELOCITY = -250.0
+
+@onready var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta: float) -> void:
